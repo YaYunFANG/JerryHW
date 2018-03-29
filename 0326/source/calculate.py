7d8c1e8dce9d8ad473c049d7c127f8e5fbf06a28
YaYunFANG <37055200+YaYunFANG@users.noreply.github.com>
2018-03-29 22:20:15 +0800

    0326

---
+++ 0326/source/calculate.py
@@ -0,0 +1,15 @@
+
+# coding: utf-8
+
+# In[2]:
+
+
+def compete_sum(a1,a2):
+    return a1+a2
+def compete_minus(a1,a2):
+    return a1-a2
+def compete_times(a1,a2):
+    return a1*a2
+def compete_divide(a1,a2):
+    return a1/a2
+
